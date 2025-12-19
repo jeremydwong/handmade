dfdb5b6779497c130552f118978b3576470a1d4f
Michael Messier <michael.g.messier@gmail.com>
2025-12-19 13:26:35 -0700

    day 3.

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,188 @@
+/* ========================================================================
+   $File: $
+   $Date: $
+   $Revision: $
+   $Creator: Casey Muratori $
+   $Notice: (C) Copyright 2014 by Molly Rocket, Inc. All Rights Reserved. $
+   ======================================================================== */
+
+#include <windows.h>
+
+#define internal static 
+#define local_persist static 
+#define global_variable static
+
+// TODO(casey): This is a global for now.
+global_variable bool Running;
+
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory; //later we will cast this to more than one type
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void
+Win32ResizeDIBSection(int Width, int Height)
+{
+    // TODO(casey): Bulletproof this.
+    // Maybe don't free first, free after, then free first if that fails.
+
+    if(BitmapHandle)
+    {
+        DeleteObject(BitmapHandle);
+    }
+
+    if(!BitmapDeviceContext)
+    {
+        // TODO(casey): Should we recreate these under certain special circumstances
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+    
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader); //cool. here we are going to pass biSize so that we can do pointer arithmetic under the hood in CreateDIBSection
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+    // TODO(casey): Based on ssylvan's suggestion, maybe we can just
+    // allocate this ourselves?
+    
+    BitmapHandle = CreateDIBSection(
+        BitmapDeviceContext, &BitmapInfo,
+        DIB_RGB_COLORS,
+        &BitmapMemory,
+        0, 0);
+}
+
+internal void
+Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+    StretchDIBits(DeviceContext,
+                  X, Y, Width, Height,
+                  X, Y, Width, Height,
+                  BitmapMemory,
+                  &BitmapInfo,
+                  DIB_RGB_COLORS, SRCCOPY);
+                  // historical; bitblt() used to be faster on older windows versions. 
+                  // now stretchis probably okay for now;
+                  // we may want to use opengl and eventually write to
+}
+
+LRESULT CALLBACK
+Win32MainWindowCallback(HWND Window,
+                        UINT Message,
+                        WPARAM WParam,
+                        LPARAM LParam)
+{
+    LRESULT Result = 0;
+
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width, Height);
+        } break;
+
+        case WM_CLOSE:
+        {
+            // TODO(casey): Handle this with a message to the user?
+            Running = false;
+        } break;
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+        case WM_DESTROY:
+        {
+            // TODO(casey): Handle this as an error - recreate window?
+            Running = false;
+        } break;
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+            Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+            EndPaint(Window, &Paint);
+        } break;
+
+        default:
+        {
+//            OutputDebugStringA("default\n");
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+    
+    return(Result);
+}
+
+int CALLBACK
+WinMain(HINSTANCE Instance,
+        HINSTANCE PrevInstance,
+        LPSTR CommandLine,
+        int ShowCode)
+{
+    WNDCLASS WindowClass = {};
+    
+    // TODO(casey): Check if HREDRAW/VREDRAW/OWNDC still matter
+    WindowClass.lpfnWndProc = Win32MainWindowCallback;
+    WindowClass.hInstance = Instance;
+//    WindowClass.hIcon;
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    if(RegisterClassA(&WindowClass))
+    {
+        HWND WindowHandle =
+            CreateWindowExA(
+                0,
+                WindowClass.lpszClassName,
+                "Handmade Hero",
+                WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                0,
+                0,
+                Instance,
+                0);
+        if(WindowHandle)
+        {
+            Running = true;
+            while(Running)
+            {
+                MSG Message;
+                BOOL MessageResult = GetMessageA(&Message, 0, 0, 0);
+                if(MessageResult > 0)
+                {
+                    TranslateMessage(&Message);
+                    DispatchMessageA(&Message);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // TODO(casey): Logging
+        }
+    }
+    else
+    {
+        // TODO(casey): Logging
+    }
+    
+    return(0);
+}
---
+++ misc/notes.md
@@ -0,0 +1,5 @@
+notes
+
+### 2025-12-19 Day 3
+
+think about controlling data in waves. if you've ever had a program that is slow to close it is probably that you did the RAII. do not think about managing things individually, do things in waves. 
